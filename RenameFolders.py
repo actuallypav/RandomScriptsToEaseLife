45fb6c0f33735d197734120a986926689b89cfc3
actuallypav <61046893+actuallypav@users.noreply.github.com>
2022-05-27 18:34:38 +0100

    Update RenameFolders.py

--- RenameFolders.py
+++ RenameFolders.py
@@ -1,10 +1,15 @@
 import os
-rootdir = 'C:/Users/actua/Desktop/Portraits/'
+rootdir = {INSERT FILE PATH HERE}
+#what value you want to rename it to (this case it was a digit)
 x = 9
 
-
+#loop through the folders, get the root directory, the subdirectories, and the files within
 for root, subdir, file in os.walk(rootdir):
+    #only care for the folders
     for name in subdir:
+        #print the folders just in case
         print(os.path.join(root,name))
+        #increase the value so not all folders are the same
         x += 1
+        #rename folders
         os.rename(os.path.join(root,name), os.path.join(root,('00'+str(x))))
